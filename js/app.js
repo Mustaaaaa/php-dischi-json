20ec0c4fdb675488196e24a21bd60c33fb01b45c
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-05-09 16:37:42 +0200

    add: 1) creato il file server.php 2) creato il file style per personalizzare il layout 3)creato il file app.js con all'interno l'array per verificare il corretto funzionamento 4)modificato il layout nel file index(da finire) 5)collegato tutto e tutto funzionante

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello vue!',
+            songs: [],
+        }
+    },
+    methods: {
+        fetchSongs() {
+            axios.get('server.php').then((res) => {
+                console.log(res.data)
+                this.songs = res.data
+                // console.log(this.songs)
+            })
+        },
+    },
+    created() {
+        this.fetchSongs()
+    },
+}).mount('#app')
